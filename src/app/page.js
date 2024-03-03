413c693669a95d81ede30ae40c603aff9eaf5525
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2024-03-03 05:30:55 -0500

    few edits on excel

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Excelreader from "@/Component/Excelwork/Excelreader";
 
-export default function Home() {
+export default function Home() 
+{
   return (
     <div>
-      <Excelreader excelsheet_name = "../Component/210HWFeedbackTemplate.xlsx" />
+      <Excelreader />
     </div>
   );
+  
 }
